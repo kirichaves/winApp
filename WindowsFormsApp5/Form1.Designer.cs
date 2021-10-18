cbbd916a0d63dc06120b72784d4e9e0041992b24
kirichaves <92106821+kirichaves@users.noreply.github.com>
2021-10-18 20:24:45 +0300

    Add files via upload

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(35, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(275, 507);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // treeView2
+            // 
+            this.treeView2.Location = new System.Drawing.Point(444, 12);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(338, 507);
+            this.treeView2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 531);
+            this.Controls.Add(this.treeView2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView treeView2;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+        private void FillTreeNode(TreeNode driveNode, string path)
+        {
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+                foreach (string s in files)
+                {
+                    treeView2.Nodes.Clear();
+                    treeView2.Nodes.Add(s);
+                }
+              }
+         }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog()==DialogResult.OK)
+            {
+                 treeView1.Nodes.Add(folderBrowserDialog.SelectedPath);
+               
+            }
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+            e.Node.Nodes.Clear();
+            string[] dirs;
+
+
+            if (Directory.Exists(e.Node.FullPath))
+            {
+                dirs = Directory.GetDirectories(e.Node.FullPath);
+                if (dirs.Length != 0)
+                {
+                    for (int i = 0; i < dirs.Length; i++)
+                    {
+                        TreeNode dirNode = new TreeNode(new DirectoryInfo(dirs[i]).Name);
+                        FillTreeNode(dirNode, dirs[i]);
+                        e.Node.Nodes.Add(dirNode);
+
+                    }
+
+
+                }
+            }
+
+        }
+
+
+    }
+        
+
+    
+    
+    
+}
